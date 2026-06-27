6f2858d439ab87a30cc9db607fa233088c9d0012
Tim <49987073+timrosu@users.noreply.github.com>
2026-06-27 12:35:09 +0200

    ricing
    Signed-off-by: Tim <49987073+timrosu@users.noreply.github.com>

--- home/modules/cli/btop.nix
+++ home/modules/cli/btop.nix
@@ -8,7 +8,7 @@
     settings = {
       vim_keys = true;
       rounded_corners = true;
-      proc_tree = true;
+      proc_tree = false;
       show_gpu_info = "on";
       show_uptime = true;
       show_coretemp = true;
@@ -17,6 +17,8 @@
       only_physical = true;
       io_mode = true;
       io_graph_combined = false;
+      color_theme = "onedark";
+      theme_background = false;
     };
   };
 }
--- home/modules/desktop/kitty.nix
+++ home/modules/desktop/kitty.nix
@@ -5,7 +5,7 @@
     enable = true;
     font = {
       name = "JetBrainsMono Nerd Font";
-      size = 12;
+      size = 11.5;
     };
 
     settings = {
