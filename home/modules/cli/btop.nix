634fc52d175280e8c1811a0ec9dbd91b0b2ee576
Tim <49987073+timrosu@users.noreply.github.com>
2026-06-29 22:51:31 +0200

    btop tweaks
    Signed-off-by: Tim <49987073+timrosu@users.noreply.github.com>

--- home/modules/cli/btop.nix
+++ home/modules/cli/btop.nix
@@ -7,18 +7,27 @@
     };
     settings = {
       vim_keys = true;
+      update_ms = 1000;
       rounded_corners = true;
       proc_tree = false;
-      show_gpu_info = "on";
       show_uptime = true;
       show_coretemp = true;
       cpu_sensor = "auto";
+      show_cpu_watts = true;
+      freq_mode = "average";
       show_disks = true;
       only_physical = true;
       io_mode = true;
       io_graph_combined = false;
       color_theme = "onedark";
       theme_background = false;
+      truecolor = true;
+      terminal_sync = true;
+      shown_boxes = "cpu net proc";
+      proc_sorting = "cpu lazy";
+      show_battery_watts = true;
+      show_gpu_info = "on";
+      shown_gpus = "intel";
     };
   };
 }
